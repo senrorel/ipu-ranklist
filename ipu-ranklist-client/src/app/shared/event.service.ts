15bc77980c45498594af7d94f3e2a04d16ec63f1
senrorel <123095858+senrorel@users.noreply.github.com>
2023-01-19 21:03:26 +0530

    Delete ipu-ranklist-client directory

--- ipu-ranklist-client/src/app/shared/event.service.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from '@angular/core';
-
-export class EventService {
-    rowSelectedEvent = new EventEmitter<any>();
-
-    rowSelected(modalData) {
-        this.rowSelectedEvent.emit(modalData);
-    }
-}
